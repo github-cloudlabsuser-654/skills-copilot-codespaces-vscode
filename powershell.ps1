0668d89889caea9b729d8d6ca692f3dbf9cc017c
github-cloudlabsuser-654 <167427289+github-cloudlabsuser-654@users.noreply.github.com>
2024-06-26 10:35:00 +0000

    Copilot third commit

---
+++ powershell.ps1
@@ -0,0 +1,13 @@
+# Login to Azure
+Connect-AzAccount
+
+# Set variables
+$resourceGroupName = "myResourceGroup"
+$location = "West Europe"
+$storageAccountName = "mystorageaccount"
+
+# Create a resource group
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+# Create a storage account
+New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -Location $location -SkuName Standard_LRS
